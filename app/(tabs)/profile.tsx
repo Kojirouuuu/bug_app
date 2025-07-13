62fe615357f98d19269ebbd173390eb055e7e3ac
Kojirouuuu <k5mikanmikan@gmail.com>
2025-07-13 11:39:15 +0900

    add:assets

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -8,6 +8,7 @@ import {
   ScrollView,
   Alert,
   Image,
+  ActivityIndicator,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
